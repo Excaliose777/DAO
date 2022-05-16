756043f4ba46d335478ada4ce130c79810d076fb
AlaminxAb <80631704+Excaliose777@users.noreply.github.com>
2022-05-16 22:39:36 +0100

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -388,7 +388,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
+        Made by Excaliose 
       </footer>
     </div>
   );
